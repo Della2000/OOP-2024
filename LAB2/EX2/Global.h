dcb30b6e2f4152f539d46e7e1fff69ea7b849eae
Della2000 <125904500+Della2000@users.noreply.github.com>
2024-03-10 18:40:33 +0200

    Update Global.h

--- LAB2/EX2/Global.h
+++ LAB2/EX2/Global.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "Student.h"
+
+
+char NameCompare(StudentInfo* std1, StudentInfo* std2);
+float MathCompare(StudentInfo* std1, StudentInfo* std2);
+float EngCompare(StudentInfo* std1, StudentInfo* std2);
+float HistCompare(StudentInfo* std1, StudentInfo* std2);
+float AvrgCompare(StudentInfo* std1, StudentInfo* std2);
