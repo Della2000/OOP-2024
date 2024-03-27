e055b7bfdf76e6a52d709afd2f3eaa283b1d094f
Della2000 <125904500+Della2000@users.noreply.github.com>
2024-03-27 15:40:01 +0200

    Update Sort .h

--- LAB4/Sort .h
+++ LAB4/Sort .h
@@ -1,28 +1,20 @@
 #pragma once
 class Sort
 {
-    // add data members
-    int* vector;
-    int count;
-    int nr_elem;
+    int* m_vector;
+    int m_count;
+    int m_nr_elemente;
+    int m_index;
 
 public:
-    //create the list that needs to be sorted out of random values within a specific interval(min, max).
-    //The constructor will receive 3 parameters(the number of elements in the list, minimum value, maximum value).
     Sort(int nr_elemente, int max_elemente, int min_elemente);
 
-    //create the list that needs to be sorted from an initialization list
     Sort(int* estera, int nr_elemente);
 
-    //create the list that needs to be sorted from an existing
-    //vector(the constructor will have two parameters - one being the vector, the other one being the number of elements from the vector)
     Sort(int count);
 
-    //create the list that needs to be sorted using variadic parameters(use va_args for this)
     Sort(int count, ...);
 
-    //create the list that needs to be sorted from a string(e.g. "10,40,100,5,70"->each number is separated 
-    //from the rest of the number with a comma).It is assumed that the string is correctly written(no space, only numerical charactersand commas)
     Sort(char* c);
 
     void InsertSort(bool ascendent = false);
@@ -32,4 +24,3 @@ public:
     int  GetElementsCount();
     int  GetElementFromIndex(int index);
 };
-
